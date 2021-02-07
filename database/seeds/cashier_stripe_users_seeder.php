81eaa3f592415ea516164fbbe3d724790c1e8230
Animesh Kumar <animesh.constacloud@gmail.com>
2021-02-07 11:55:26 +0530

    Cashier-Stripe Implementation

--- database/seeds/cashier_stripe_users_seeder.php
+++ database/seeds/cashier_stripe_users_seeder.php
@@ -13,6 +13,6 @@ class cashier_stripe_users_seeder extends Seeder
      */
     public function run()
     {
-        $cashier_stripe_user = cashier_stripe_user::create(array("cashier_stripe_users_name" => "Super Admin", "cashier_stripe_users_userid" => "superadmin@test.com", "cashier_stripe_users_email" => "superadmin@test.com", "cashier_stripe_users_password" => CommonHelper::encrip_password("test@321", "e"), "cashier_stripe_users_isactive" => 1, "cashier_stripe_users_isdeleted" => 0, "cashier_stripe_users_created_at" => new Carbon, "cashier_stripe_users_updated_at" => new Carbon, "cashier_stripe_users_forget_password_requested" => null, "cashier_stripe_users_forget_password_link" => null, "cashier_stripe_users_forget_password_link_status" => null, "cashier_stripe_users_forget_password_link_expiration_time" => null ));
+        $cashier_stripe_user = cashier_stripe_user::create(array("cashier_stripe_users_name" => "Super Admin", "cashier_stripe_users_userid" => "superadmin@test.com", "cashier_stripe_users_email" => "superadmin@test.com", "cashier_stripe_users_password" => CommonHelper::encrip_password("test@321", "e"), "cashier_stripe_users_isactive" => 1, "cashier_stripe_users_isdeleted" => 0, "cashier_stripe_users_created_at" => new Carbon, "cashier_stripe_users_updated_at" => new Carbon, "cashier_stripe_users_forget_password_requested" => 1, "cashier_stripe_users_forget_password_link" => null, "cashier_stripe_users_forget_password_link_status" => null, "cashier_stripe_users_forget_password_link_expiration_time" => null ));
     }
 }
